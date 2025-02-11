bb2eb0dedbd86c0b05b7029b715e10bf61acc027
calypsoVALID <1105340032@qq.com>
2025-02-11 13:54:39 +0800

    disc01 Done

---
+++ disc01/disc01.py
@@ -0,0 +1,103 @@
+def race(x, y):
+    """The tortoise always walks x feet per minute, while the hare repeatedly
+    runs y feet per minute for 5 minutes, then rests for 5 minutes. Return how
+    many minutes pass until the tortoise first catches up to the hare.
+
+    >>> race(5, 7)  # After 7 minutes, both have gone 35 steps
+    7
+    >>> race(2, 4) # After 10 minutes, both have gone 20 steps
+    10
+    """
+    assert y > x and y <= 2 * x, 'the hare must be fast but not too fast'
+    tortoise, hare, minutes = 0, 0, 0
+    while minutes == 0 or tortoise - hare:
+        tortoise += x
+        if minutes % 10 < 5:
+            hare += y
+        minutes += 1
+    return minutes
+
+def fizzbuzz(n):
+    """
+    >>> result = fizzbuzz(16)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    16
+    >>> print(result)
+    None
+    """
+    "*** YOUR CODE HERE ***"
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
+            print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
+        else:
+            print(i)
+
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    >>> is_prime(1) # one is not a prime number!!
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return False
+    else:
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n.
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(101) # 0 and 1
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    count = 0
+    for i in range(10):
+        if has_digit(n, i):
+            count += 1
+    return count
+
+def has_digit(n, k):
+    """Returns whether k is a digit in n.
+
+    >>> has_digit(10, 1)
+    True
+    >>> has_digit(12, 7)
+    False
+    """
+    assert k >= 0 and k < 10
+    "*** YOUR CODE HERE ***"
+    while n > 0:
+        if n % 10 == k:
+            return True
+        n = n // 10
+    return False
+
